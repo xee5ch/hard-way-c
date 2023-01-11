47f35fc949f5ca10a7170e1fb7ad3780f078e98c
Al S <xee5ch.gh.al@il5.in>
2023-01-10 23:37:45 -0500

    Add Exercise 1.

---
+++ ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    // Use printf d% directive for printing a number
+    printf("You are %d miles away.\n", distance);
+    return 0;
+}
